c7618356fea609f6e9d54a0052fe842c2ded6ff6
Filipe <moscovo.filipe@gmail.com>
2023-09-02 15:48:10 -0600

    Add search bar with logic to prompt user to add a pin to the map

--- frontend/src/components/GoogleMapComponent.js
+++ frontend/src/components/GoogleMapComponent.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { GoogleMap, LoadScript, InfoWindow, Marker } from '@react-google-maps/api';
+import React, { useState, useEffect, useRef } from 'react';
+import { GoogleMap, LoadScript, InfoWindow, Marker, StandaloneSearchBox } from '@react-google-maps/api';
 import mapStyles from './mapStyles';
 
 const containerStyle = {
@@ -8,16 +8,16 @@ const containerStyle = {
 };
 
 /**
- * `GoogleMapComponent` is a React component that renders an interactive Google Map.
+ * Renders a Google Map component with the following features:
+ * 1. Displays markers filtered by topics.
+ * 2. Adds new markers by clicking on the map.
+ * 3. Shows an info window on marker hover.
+ * 4. Provides a search box to search and center the map on specific addresses or points of interest.
  *
- * The map initializes to the user's geolocation (if available) and allows for markers
- * to be added by clicking on any location. These markers can be filtered based on
- * predefined topics. Interactions with the map are handled in a declarative manner
- * using React's state management, ensuring optimal DOM updates.
+ * Interacts with the DOM primarily through React's virtual DOM, with effects causing direct DOM mutations.
  *
- * @returns {JSX.Element} A Google Map wrapped within UI controls for topic filtering and marker addition.
+ * @returns {JSX.Element} The main GoogleMapComponent and associated components.
  */
-
 const GoogleMapComponent = () => {
   const [center, setCenter] = useState({ lat: -3.745, lng: -38.523 });
   const [selectedMarker, setSelectedMarker] = useState(null);
@@ -28,6 +28,8 @@ const GoogleMapComponent = () => {
   const [showInputForm, setShowInputForm] = useState(false);
   const [clickedLocation, setClickedLocation] = useState(null);
   const [hideTimeout, setHideTimeout] = useState(null);
+  const [searchBox, setSearchBox] = useState(null);
+  const mapRef = useRef(null);
 
   useEffect(() => {
     if ("geolocation" in navigator) {
@@ -67,11 +69,25 @@ const GoogleMapComponent = () => {
     setMarkers(prevMarkers => [...prevMarkers, marker]);
   };
 
-  const handleMapClick = event => {
-    const pixelX = event.pixel.x;
-    const pixelY = event.pixel.y;
-    setClickedLocation({ lat: event.latLng.lat(), lng: event.latLng.lng(), pixelX, pixelY });
-    setShowInputForm(true);
+  const onPlacesChanged = () => {
+    const places = searchBox.getPlaces();
+    if (places && places.length === 1) {
+      const place = places[0];
+      const location = place.geometry.location;
+      setCenter({
+        lat: location.lat(),
+        lng: location.lng(),
+      });
+      mapRef.current.panTo(location);
+    }
+};
+
+
+const handleMapClick = event => {
+  const pixelX = event.pixel.x;
+  const pixelY = event.pixel.y;
+  setClickedLocation({ lat: event.latLng.lat(), lng: event.latLng.lng(), pixelX, pixelY });
+  setShowInputForm(true);
 };
 
   const handleSubmit = () => {
@@ -83,95 +99,140 @@ const GoogleMapComponent = () => {
   };
 
   return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_MAPSKEY}>
-      <div>
-        <select
-          value={visibleTopic}
-          onChange={(e) => setVisibleTopic(e.target.value)}
-          style={{ marginBottom: '10px' }}
-        >
-          <option value="all">Show All</option>
-          <option value="business">Business</option>
-          <option value="garage">Garage</option>
-          <option value="gathering">Gathering</option>
-        </select>
+    <LoadScript googleMapsApiKey={process.env.REACT_APP_MAPSKEY} libraries={['places']}>
+        <div>
+            <select
+                value={visibleTopic}
+                onChange={(e) => setVisibleTopic(e.target.value)}
+                style={{ marginBottom: '10px' }}
+            >
+                <option value="all">Show All</option>
+                <option value="business">Business</option>
+                <option value="garage">Garage</option>
+                <option value="gathering">Gathering</option>
+            </select>
 
-        {center && (
-          <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={15}
-            options={{ styles: mapStyles }}
-            onClick={handleMapClick}
-          >
-            {markers
-              .filter(marker => visibleTopic === 'all' || marker.metadata.topic === visibleTopic)
-              .map((marker, idx) => (
-                <Marker
-                key={idx}
-                position={marker.position}
-                icon={marker.icon}
-                onMouseOver={() => {
-                  if (hideTimeout) {
-                    clearTimeout(hideTimeout);
-                    setHideTimeout(null);
-                  }
-                  setSelectedMarker(marker);
-                  setInfoWindowVisible(true);
-                }}
-                onMouseOut={() => {
-                  const timeout = setTimeout(() => {
-                    setInfoWindowVisible(false);
-                  }, 1300);  // Delay. Adjust as needed.
-                  setHideTimeout(timeout);
-                }}
-              />
-            ))}
+            {center && (
+                <GoogleMap
+                    mapContainerStyle={containerStyle}
+                    center={center}
+                    zoom={15}
+                    options={{ styles: mapStyles }}
+                    onClick={handleMapClick}
+                    onLoad={map => {
+                        mapRef.current = map;
 
-            {infoWindowVisible && selectedMarker && (
-              <InfoWindow
-                position={selectedMarker.position}
-                onCloseClick={() => setInfoWindowVisible(false)}
-              >
-                <div>
-                  <h4>{selectedMarker.metadata.topic}</h4>
-                </div>
-              </InfoWindow>
+                        // Create the input element for the search box
+                        const input = document.createElement("input");
+                        input.type = "text";
+                        input.placeholder = "Search for a place...";
+                        input.style.boxSizing = 'border-box';
+                        input.style.border = '1px solid transparent';
+                        input.style.width = '240px';
+                        input.style.height = '32px';
+                        input.style.padding = '0 12px';
+                        input.style.borderRadius = '3px';
+                        input.style.boxShadow = '0 2px 6px rgba(0, 0, 0, 0.3)';
+                        input.style.fontSize = '14px';
+                        input.style.outline = 'none';
+                        input.style.textOverflow = 'ellipses';
+
+                       // Bind the StandaloneSearchBox to the input element
+const searchBox = new window.google.maps.places.SearchBox(input);
+searchBox.addListener('places_changed', () => {
+    const places = searchBox.getPlaces();
+    if (places.length === 1) {
+        const place = places[0];
+        const location = {
+            lat: place.geometry.location.lat(),
+            lng: place.geometry.location.lng()
+        };
+
+        // Set map center and pan to the location
+        setCenter(location);
+        mapRef.current.panTo(location);
+
+        // Prompt user for a topic
+        const topic = prompt("Enter topic (business, garage, gathering):");
+        if (topic) {
+            // Add a new marker at the location with the provided topic
+            addMarker(location, topic);
+        }
+    }
+});
+
+                        // Position the search box at the top center of the map
+                        map.controls[window.google.maps.ControlPosition.TOP_CENTER].push(input);
+                    }}
+                >
+                    {markers
+                        .filter(marker => visibleTopic === 'all' || marker.metadata.topic === visibleTopic)
+                        .map((marker, idx) => (
+                            <Marker
+                                key={idx}
+                                position={marker.position}
+                                icon={marker.icon}
+                                onMouseOver={() => {
+                                    if (hideTimeout) {
+                                        clearTimeout(hideTimeout);
+                                        setHideTimeout(null);
+                                    }
+                                    setSelectedMarker(marker);
+                                    setInfoWindowVisible(true);
+                                }}
+                                onMouseOut={() => {
+                                    const timeout = setTimeout(() => {
+                                        setInfoWindowVisible(false);
+                                    }, 1300);  // Delay. Adjust as needed.
+                                    setHideTimeout(timeout);
+                                }}
+                            />
+                        ))}
+
+                    {infoWindowVisible && selectedMarker && (
+                        <InfoWindow
+                            position={selectedMarker.position}
+                            onCloseClick={() => setInfoWindowVisible(false)}
+                        >
+                            <div>
+                                <h4>{selectedMarker.metadata.topic}</h4>
+                            </div>
+                        </InfoWindow>
+                    )}
+                </GoogleMap>
             )}
-          </GoogleMap>
-        )}
 
-{showInputForm && clickedLocation && (
-          <div
-            style={{
-              position: 'absolute',
-              top: `${clickedLocation.pixelY}px`,
-              left: `${clickedLocation.pixelX}px`,
-              background: 'white',
-              padding: '10px'
-            }}
-          >
-            <label>
-              Enter topic (business, garage, gathering):
-              <input
-                type="text"
-                value={inputValue}
-                onChange={e => setInputValue(e.target.value)}
-                list="topics"
-              />
-            </label>
-            <datalist id="topics">
-              <option value="business" />
-              <option value="garage" />
-              <option value="gathering" />
-            </datalist>
-            <button onClick={handleSubmit}>Add Marker</button>
-            <button onClick={() => setShowInputForm(false)}>Cancel</button>
-          </div>
-        )}
-      </div>
+            {showInputForm && clickedLocation && (
+                <div
+                    style={{
+                        position: 'absolute',
+                        top: `${clickedLocation.pixelY}px`,
+                        left: `${clickedLocation.pixelX}px`,
+                        background: 'white',
+                        padding: '10px'
+                    }}
+                >
+                    <label>
+                        Enter topic (business, garage, gathering):
+                        <input
+                            type="text"
+                            value={inputValue}
+                            onChange={e => setInputValue(e.target.value)}
+                            list="topics"
+                        />
+                    </label>
+                    <datalist id="topics">
+                        <option value="business" />
+                        <option value="garage" />
+                        <option value="gathering" />
+                    </datalist>
+                    <button onClick={handleSubmit}>Add Marker</button>
+                    <button onClick={() => setShowInputForm(false)}>Cancel</button>
+                </div>
+            )}
+        </div>
     </LoadScript>
-  );
+);
 };
 
 export default GoogleMapComponent;
