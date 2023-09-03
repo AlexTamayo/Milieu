8dcf218ba4cf71e90e36aed2796ebf0acce3c98b
Alex Tamayo <catd00@gmail.com>
2023-09-03 03:25:35 -0700

    Start of top nav bar

--- frontend/src/components/TopNavBar.jsx
+++ frontend/src/components/TopNavBar.jsx
@@ -1,9 +1,46 @@
-// import React from 'react';
+import { useState } from "react";
+
 import '../styles/TopNavBar.scss';
+import logo from '../assets/logo/logo.svg'
+import logo_and_name from '../assets/logo/logo-and-name.svg'
+
+import profile from '../assets/temp/profile_pics_6.png'
+// import logo from '../assets/logo/logo.png'
+
+import PlusSign from './PlusSign';
+
 
 function TopNavBar() {
+
+  const [logged, setlogged] = useState(true);
+
+
   return (
-    <div className="top-nav-bar">Top nav</div>
+    <div className="top-nav-bar">
+      <div className='top-nav-bar__left'>
+        <div className='left__logo'>
+        <img src={logo_and_name} alt="logo" />
+        </div>
+      </div>
+
+      {logged ?
+        <div className='top-nav-bar__right'>
+          <div className='right__add'>
+            {/* <img src={logo} alt="logo" /> */}
+            < PlusSign />
+          </div>
+          <div className='right__user'>
+            <img src={profile} alt="" /> 
+          </div>
+        </div>
+      : <div className='top-nav-bar__right'>
+          <div>
+            <strong>Log In</strong>
+          </div>
+        </div>
+      }
+
+    </div>
   );
 }
 
