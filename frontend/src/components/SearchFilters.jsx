2674296f0b9c5e9bf7d6df9538f26dd1184b2efb
Alex Tamayo <catd00@gmail.com>
2023-09-05 02:44:56 -0700

    Update search bar looks

---
+++ frontend/src/components/SearchFilters.jsx
@@ -0,0 +1,24 @@
+import '../styles/SearchFilters.scss';
+
+function SearchFilters() {
+
+
+  return (
+    <div className="search-filters">
+      <div className="filter filter--businesses">
+        Businesses
+      </div>
+      <div className="filter filter--events">
+        Events
+      </div>
+      <div className="filter filter--test">
+        Test 1
+      </div>
+      <div className="filter filter--test2">
+        Test 2
+      </div>
+    </div>
+  );
+}
+
+export default SearchFilters;
