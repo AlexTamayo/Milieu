0ec425e183d5b6c5ae1eeef604500b66e8c9a603
Alex Tamayo <catd00@gmail.com>
2023-09-06 14:42:48 -0700

    Modify UserModalContext

--- frontend/src/utils/UserModalContext.js
+++ frontend/src/utils/UserModalContext.js
@@ -1,12 +1,5 @@
 import { createContext, useState, useEffect } from 'react';
 
-// export const UserModalContext = createContext({
-//   isUserModalOpen: false,
-//   openUserModal: () => {},
-//   closeUserModal: () => {}
-// });
-
-
 export const UserModalContext = createContext();
 
 export const UserModalProvider = ({ children }) => {
