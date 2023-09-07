381c49682a103b101deb4df7761cca8bcdd93949
Alex Tamayo <catd00@gmail.com>
2023-09-06 21:51:57 -0700

    Modify search bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,19 @@ import './App.scss';
 
 import { UserModalProvider } from "./context/UserModalContext";
 import { DataProvider } from './context/dataProviderContext';
+
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faMapPin } from '@fortawesome/free-solid-svg-icons';
 
 import GoogleMapComponent from './components/GoogleMapComponent';
 import GoogleMapComponent2 from './components/GoogleMapComponent2';
+
+// Temp organisation for AlexT stuff:
 import TopNavBar from './components/TopNavBar';
 import SearchBar from './components/SearchBar';
 import UserModal from './components/UserModal';
-import TestComponent from './components/TestComponent';
+import VenueModal from './components/VenueModal';
+
 
 library.add(faMapPin);
 
@@ -21,22 +25,20 @@ function App() {
   const [isUserModalOpen, setisUserModalOpen] = useState(false);
 
   const userDivRef = useRef(null);
-
+  
   return (
-    <DataProvider >
-      < UserModalProvider >
-      <div className="App">
-        < UserModal userDivRef={userDivRef} />
-        < SearchBar />
-        < TopNavBar userDivRef={userDivRef} />
-        {/* < GoogleMapComponent2 /> */}
-        {/* < TopNavBar /> */}
-        < GoogleMapComponent />
-        <TestComponent />
-          {/* Other components */}
-      </div>
-      </ UserModalProvider >
-    </DataProvider>
+    < UserModalProvider >
+    <div className="App">
+      < VenueModal />
+      < UserModal userDivRef={userDivRef} />
+      < SearchBar />
+      < TopNavBar userDivRef={userDivRef} />
+      < GoogleMapComponent2 />
+      {/* < TopNavBar /> */}
+      {/* < GoogleMapComponent /> */}
+      {/* < ParseBusinessAndEventData /> */}
+    </div>
+    </ UserModalProvider >
   );
 }
 
