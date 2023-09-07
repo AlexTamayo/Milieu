70b9fc2149060775d5047bcb601e0ef30930eb99
Alex Tamayo <catd00@gmail.com>
2023-09-06 19:28:07 -0700

    Create venue modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import React, { useState, useRef } from 'react';
 import './App.scss';
 
 import { UserModalProvider } from "./context/UserModalContext";
-
+import { DataProvider } from './context/dataProviderContext';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faMapPin } from '@fortawesome/free-solid-svg-icons';
 
@@ -11,12 +11,8 @@ import GoogleMapComponent from './components/GoogleMapComponent';
 import GoogleMapComponent2 from './components/GoogleMapComponent2';
 import TopNavBar from './components/TopNavBar';
 import SearchBar from './components/SearchBar';
-import ParseBusinessAndEventData from './hooks/parseBusinessAndEventData';
-
-
-
-
 import UserModal from './components/UserModal';
+import TestComponent from './components/TestComponent';
 
 library.add(faMapPin);
 
@@ -25,20 +21,22 @@ function App() {
   const [isUserModalOpen, setisUserModalOpen] = useState(false);
 
   const userDivRef = useRef(null);
-  
+
   return (
-    < UserModalProvider >
-    <div className="App">
-      < UserModal userDivRef={userDivRef} />
-      < SearchBar />
-      < TopNavBar userDivRef={userDivRef} />
-      < GoogleMapComponent2 />
-      < TopNavBar />
-      < GoogleMapComponent />
-      < ParseBusinessAndEventData />
-      {/* Other components */}
-    </div>
-    </ UserModalProvider >
+    <DataProvider >
+      < UserModalProvider >
+      <div className="App">
+        < UserModal userDivRef={userDivRef} />
+        < SearchBar />
+        < TopNavBar userDivRef={userDivRef} />
+        < GoogleMapComponent2 />
+        < TopNavBar />
+        < GoogleMapComponent />
+        <TestComponent />
+          {/* Other components */}
+      </div>
+      </ UserModalProvider >
+    </DataProvider>
   );
 }
 
--- frontend/src/components/TopNavBar.jsx
+++ frontend/src/components/TopNavBar.jsx
@@ -6,16 +6,12 @@ import '../styles/TopNavBar.scss';
 import logo_and_name from '../assets/logo/logo-and-name.svg'
 import profile from '../assets/temp/profile_pics_6.png'
 
-
 import PlusSign from './SVGs/PlusSign';
-
 
 function TopNavBar({ userDivRef }) {
   const { toggleUserModal } = useContext(UserModalContext);
 
   const [logged, setlogged] = useState(true);
-
-  
 
   return (
     <div className="top-nav-bar">
