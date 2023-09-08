1a21475bb6c941aa4be73b3c716ea898d8c4c5a1
Alex Tamayo <catd00@gmail.com>
2023-09-08 07:26:13 -0700

    Delete old database files

--- backend/old_db_attempt/syncDb.js
+++
@@ -1,9 +0,0 @@
-const db = require('./models');
-
-db.sequelize.sync()
-  .then(() => {
-    console.log('Database & tables created!');
-  })
-  .catch(error => {
-    console.error("Error creating tables:", error);
-  });
