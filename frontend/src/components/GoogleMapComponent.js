c00f8d1493f1539a4b0457a4fae66d507099945f
Filipe <moscovo.filipe@gmail.com>
2023-09-10 02:19:44 -0600

    Custom Icons Logic and code improvement

--- frontend/src/components/GoogleMapComponent.js
+++ frontend/src/components/GoogleMapComponent.js
@@ -3,91 +3,111 @@ import {
   GoogleMap,
   LoadScript,
   Marker,
+  MarkerClusterer,
 } from '@react-google-maps/api';
 import { DataContext } from '../context/MainContext';
 import mapStyles from '../styles/mapStyles.js';
+import categoryIcons from '../routes/categoryIcons';
 
 const containerStyle = {
   width: '100%',
   height: '800px',
 };
 
+const ICON_SIZE = { width: 40, height: 40 };
+
 const GoogleMapComponent = () => {
+  const [isGoogleMapLoaded, setIsGoogleMapLoaded] = useState(false);
   const [center, setCenter] = useState({ lat: -3.745, lng: -38.523 });
   const [markers, setMarkers] = useState([]);
   const { state } = useContext(DataContext);
   const { eventData, businessData, selectedFilter } = state;
+  const mapRef = React.useRef(null);
 
   useEffect(() => {
-    const newMarkers = [];
+    if (isGoogleMapLoaded && mapRef.current) {
+      const newMarkers = [];
+      const iconSize = new window.google.maps.Size(ICON_SIZE.width, ICON_SIZE.height);
 
-    if (!selectedFilter || selectedFilter === 'events') {
-      eventData.forEach((event) => {
-        if (event.eventLocation && event.eventLocation.latitude && event.eventLocation.longitude) {
-          newMarkers.push({
-            position: { lat: event.eventLocation.latitude, lng: event.eventLocation.longitude },
-            metadata: { topic: 'event', name: event.title, link: event.link },
-            icon: {
-              url: "http://maps.google.com/mapfiles/ms/micons/sportvenue.png" // New icon for events
-            },
-          });
-        }
-      });
+      if (!selectedFilter || selectedFilter === 'events') {
+        eventData.forEach((event) => {
+          if (event.eventLocation && event.eventLocation.latitude && event.eventLocation.longitude) {
+            newMarkers.push({
+              position: { lat: event.eventLocation.latitude, lng: event.eventLocation.longitude },
+              metadata: { topic: 'event', name: event.title, link: event.link },
+              icon: {
+                url: categoryIcons[event.categoryName] || "http://maps.google.com/mapfiles/ms/micons/sportvenue.png",
+                scaledSize: iconSize,
+              },
+            });
+          }
+        });
+      }
+
+      if (!selectedFilter || selectedFilter === 'businesses') {
+        businessData.forEach((business) => {
+          if (business.businessLocation && business.businessLocation.latitude && business.businessLocation.longitude) {
+            newMarkers.push({
+              position: { lat: business.businessLocation.latitude, lng: business.businessLocation.longitude },
+              metadata: { topic: 'business', name: business.name, link: business.website },
+              icon: {
+                url: categoryIcons[business.businessCategory.name] || "http://maps.google.com/mapfiles/kml/pal2/icon10.png",
+                scaledSize: iconSize,
+              },
+            });
+          }
+        });
+      }
+
+      setMarkers(prevMarkers => [
+        ...prevMarkers.filter(marker => marker.metadata && marker.metadata.topic === 'user-location'),
+        ...newMarkers,
+      ]);
     }
-
-    if (!selectedFilter || selectedFilter === 'businesses') {
-      businessData.forEach((business) => {
-        if (business.businessLocation && business.businessLocation.latitude && business.businessLocation.longitude) {
-          newMarkers.push({
-            position: { lat: business.businessLocation.latitude, lng: business.businessLocation.longitude },
-            metadata: { topic: 'business', name: business.name, link: business.link },
-            icon: {
-              url: "http://maps.google.com/mapfiles/kml/pal2/icon10.png" // New icon for business
-            },
-          });
-        }
-      });
-    }
-
-    setMarkers(prevMarkers => [
-      ...prevMarkers.filter(marker => marker.metadata && marker.metadata.topic === 'user-location'),
-      ...newMarkers,
-    ]);
-
-  }, [eventData, businessData, selectedFilter]);
+  }, [selectedFilter, eventData, businessData, isGoogleMapLoaded]);
 
   useEffect(() => {
-    if ("geolocation" in navigator) {
+    if (isGoogleMapLoaded && "geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(
         position => {
           const { latitude, longitude } = position.coords;
           setCenter({ lat: latitude, lng: longitude });
 
-          setMarkers(prevMarkers => [
-            ...prevMarkers.filter(marker => marker.metadata && marker.metadata.topic !== 'user-location'),
-            {
-              position: { lat: latitude, lng: longitude },
-              metadata: { topic: 'user-location', name: 'Current Location', link: '#' },
-              icon: {
-                url: "http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_gray.png" // New icon for user-location
+          if (mapRef.current) {
+            const iconSize = new window.google.maps.Size(ICON_SIZE.width, ICON_SIZE.height);
+
+            setMarkers(prevMarkers => [
+              ...prevMarkers.filter(marker => marker.metadata && marker.metadata.topic !== 'user-location'),
+              {
+                position: { lat: latitude, lng: longitude },
+                metadata: { topic: 'user-location', name: 'Current Location', link: '#' },
+                icon: {
+                  url: process.env.PUBLIC_URL + "/icons/you-are-here-icon.png",
+                  scaledSize: iconSize,
+                },
               },
-            },
-          ]);
+            ]);
+          }
         },
         error => {
           console.error('Geolocation Error:', error);
           setCenter({ lat: -3.745, lng: -38.523 });
-        }
+        },
+        { enableHighAccuracy: true }
       );
     } else {
       console.log("Geolocation not supported");
       setCenter({ lat: -3.745, lng: -38.523 });
     }
-  }, []);
+  }, [isGoogleMapLoaded]);
 
   return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_MAPSKEY}>
+    <LoadScript
+      googleMapsApiKey={process.env.REACT_APP_MAPSKEY}
+      onLoad={() => setIsGoogleMapLoaded(true)}
+    >
       <GoogleMap
+        ref={mapRef}
         mapContainerStyle={containerStyle}
         center={center}
         zoom={15}
@@ -96,17 +116,38 @@ const GoogleMapComponent = () => {
           styles: mapStyles,
         }}
       >
-        {markers.map((marker, index) => (
-  <Marker
-    key={index}
-    position={marker.position}
-    icon={marker.icon}
-    onClick={() => {
-      console.log(marker.metadata);
-    }}
-  />
-))}
-
+        <MarkerClusterer
+          key={selectedFilter}
+          options={{
+            imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+            gridSize: 50,
+            minimumClusterSize: 2,
+          }}
+        >
+          {(clusterer) =>
+            markers
+              .filter((marker) => {
+                if (selectedFilter === 'events') {
+                  return marker.metadata.topic === 'event';
+                } else if (selectedFilter === 'businesses') {
+                  return marker.metadata.topic === 'business';
+                } else {
+                  return true;
+                }
+              })
+              .map((marker, index) => (
+                <Marker
+                  key={index}
+                  position={marker.position}
+                  icon={marker.icon}
+                  onClick={() => {
+                    console.log(marker.metadata);
+                  }}
+                  clusterer={clusterer}
+                />
+              ))
+          }
+        </MarkerClusterer>
       </GoogleMap>
     </LoadScript>
   );
---
+++ frontend/src/routes/categoryIcons.js
@@ -0,0 +1,43 @@
+const categoryIcons = {
+  Restaurant: "/icons/burger.png",
+};
+
+export default categoryIcons;
+
+
+// const categoryIcons = {
+//   Healthcare: {
+//     categories: ["Hospital", "Pharmacy", "Clinic"],
+//     icon: "frontend/public/icons/toilets_inclusive.png",
+//   },
+//   PersonalCare: {
+//     categories: ["Hair Salon", "Nail Salon", "Spa"],
+//     icon: "frontend/public/icons/barber.png",
+//   },
+//   FoodAndBeverage: {
+//     categories: ["Fast Food Outlet", "Restaurant", "Café"],
+//     icon: "frontend/public/icons/burger.png",
+//   },
+//   Education: {
+//     categories: ["Tutoring Center", "Language School", "Training Center"],
+//     icon: "frontend/public/icons/cramschool.png",
+//   },
+//   ProfessionalServices: {
+//     categories: ["Legal Services", "Consulting", "Finance"],
+//     icon: "frontend/public/icons/contract.png",
+//   },
+//   ITAndTechnology: {
+//     categories: ["Computer Service", "Tech Support", "Software Development"],
+//     icon: "frontend/public/icons/computers.png",
+//   },
+//   CommunityAndWelfare: {
+//     categories: ["Community Center", "Welfare Room", "Public Services"],
+//     icon: "frontend/public/icons/welfareroom.png",
+//   },
+//   BeautyAndWellness: {
+//     categories: ["Gym", "Yoga Center", "Wellness Center"],
+//     icon: "frontend/public/icons/icon-nailsalon.png",
+//   },
+// };
+
+// export default categoryIcons;
