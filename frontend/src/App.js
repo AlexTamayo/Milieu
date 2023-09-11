d45658703954e483e4257088feab0894f2e25bef
Filipe <moscovo.filipe@gmail.com>
2023-09-11 02:14:21 -0600

    revived GoogleMapComponent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,6 +23,7 @@ function App() {
   return (
 
       <div className="App">
+        < TopNavBar userDivRef={userDivRef} />
         < UserLogRegModal />
         < UserAddVenue />
         < VenueModal />
